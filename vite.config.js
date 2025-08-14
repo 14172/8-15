617048241c4d47df02b9b414b6ed2c6c8bc1aca9
ryckie wong <ricky600900@gmail.com>
2025-08-15 02:28:43 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/8-15/',
+  base: 'https://github.com/14172/8-15',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
